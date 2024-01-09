4e923613a17bb499413dfbf4a04135c914d86bde
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-01-09 10:08:08 +0100

    Add setupMirageTest(hooks) to test
    Fixes https://github.com/rgaiacs/ember-demo-graphql/issues/1

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'learn-test/tests/helpers';
+import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | index', function (hooks) {
   setupApplicationTest(hooks);
+  setupMirageTest(hooks);
 
   test('visiting /', async function (assert) {
     await visit('/');
